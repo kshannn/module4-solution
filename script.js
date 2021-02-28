8197834360ae95d4b29b788a419a5c4d2c0f98f4
kshannn <77404139+kshannn@users.noreply.github.com>
2021-02-28 17:05:27 +0800

    added additional = for string

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var i = 0; i < names.length; i++) {
   var name = names[i];
   var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter == "j") {
+  if (firstLetter === "j") {
     byeSpeaker.speak(name);
   } 
 
